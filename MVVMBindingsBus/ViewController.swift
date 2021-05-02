7f3defca5a28c6381f23e9de6c5fa9ac15cb5875
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-02 10:53:15 +0200

    Initial Commit

---
+++ MVVMBindingsBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBindingsBus
+//
+//  Created by Ndayisenga Jean Claude on 02/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
